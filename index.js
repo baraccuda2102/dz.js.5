3b5a66e63443bc14581e4e1286eecd734b5591e4
baraccuda2102 <122445077+baraccuda2102@users.noreply.github.com>
2023-04-27 10:48:34 +0300

    dz.JS.5

---
+++ index.js
@@ -0,0 +1,11 @@
+firstNumber = +prompt('Please enter first number')
+
+secondNumber = +prompt('Please enter second number')
+
+thirdNumber = +prompt('Please enter third number')
+
+sum = firstNumber + secondNumber + thirdNumber;
+
+average = (sum/3);
+
+console.log(sum/3);
